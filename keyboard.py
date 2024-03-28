a79636de2752a44c2c54a6b46bb0a1c01812ab70
awatsonEP <awatson@extractproduction.com>
2024-03-28 12:53:52 -0500

    changed names

---
+++ keyboard.py
@@ -0,0 +1,13 @@
+from pynput import keyboard
+from time import sleep
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+    
+listener = keyboard.Listener(on_release=on_release)
+listener.start()
+
+while listener.is_alive():
+    print("hello")
+    sleep(1)
--- keyboard2.py
+++
@@ -1,8 +0,0 @@
-from pynput.keyboard import Listener
-
-def on_press(key):
-    # Handle key press events here
-    print(f"Key pressed: {key}")
-
-with Listener(on_press=on_press) as listener:
-    listener.join()
