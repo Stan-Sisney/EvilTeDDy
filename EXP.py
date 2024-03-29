ab9f9000c39cb45cb203235f60e6362c55729e5a
stan-sisney <stan.sisney@gmail.com>
2024-03-29 13:18:51 -0500

    pyglet

--- EXP.py
+++ EXP.py
@@ -1,11 +1,17 @@
+import io
+import time
+
+import pyglet
 from gtts import gTTS
-from tempfile import NamedTemporaryFile
-from playsound import playsound
 
-def speak(txt, lang='en'):
-    gTTS(text=txt, lang=lang).write_to_fp(voice := NamedTemporaryFile())
-    playsound(voice.name)
-    voice.close()
+pyglet.options["audio"] = ("pulse",)
 
-txt = "Spank me, Daddy"
-speak(txt)
+def speak(words: str, lang: str="en"):
+    with io.BytesIO() as f:
+        gTTS(text = words, lang=lang).write_to_fp(f)
+        f.seek(0)
+        
+        player = pyglet.media.load('_.mp3', file=f).play()
+        while player.playing:
+            pyglet.app.platform_event_loop.dispatch_posted_events()
+            pyglet.clock.tick()
