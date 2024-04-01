6a80ae10c68601f0f1175fa6d8649d1cccaabd5f
stan-sisney <stan.sisney@gmail.com>
2024-04-01 12:18:01 -0500

    stuff

--- EXP.py
+++ EXP.py
@@ -1,22 +1,28 @@
+import io
+#import pygame
+#from gtts import gTTS
 from pynput import keyboard
+from time import sleep
 
+#init a string var
+#stark keybopard list
+#build string up
+#hit enter pass string to gtts
+#play sound
+#esc stops everything
+
+x=""
+y=""
 def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
-
-def on_release(key):
-    print('{0} released'.format(
-        key))
-    if key == keyboard.Key.esc:
+    x=key    
+    y=(x)
+    print(y) 
+    if key == keyboard.Key.enter:
         # Stop listener
         return False
 
 # Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press,) as listener:
     listener.join()
+
+    
