7d52d5a09e249386d18770dba8e755435755a6b1
stan-sisney <stan.sisney@gmail.com>
2024-04-04 11:33:56 -0500

    init push

---
+++ Teddy Ollama.py
@@ -0,0 +1,10 @@
+import ollama
+ollama.pull('llama2')
+stream = ollama.chat(
+    model='llama2',
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    stream=True,
+)
+
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
