608b8742131ea2a31b5be3cb91718f6b18f3c413
stan-sisney <stan.sisney@gmail.com>
2024-04-09 18:11:06 -0500

    post solder job

--- Servos.py
+++ Servos.py
@@ -6,19 +6,19 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 # lower jaw
-kit.servo[0].angle = 180
+kit.servo[8].angle = 180
 time.sleep(1)
-kit.servo[0].angle = 0
+kit.servo[8].angle = 0
 time.sleep(1)
 
 #upper Jaw
-kit.servo[1].angle = 180
+kit.servo[4].angle = 180
 time.sleep(1)
-kit.servo[1].angle = 0
+kit.servo[4].angle = 0
 time.sleep(1)
 
 #eyes
-kit.servo[2].angle = 180
+kit.servo[7].angle = 180
 time.sleep(1)
-kit.servo[2].angle = 0
+kit.servo[7].angle = 0
 # Test
