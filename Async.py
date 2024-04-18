cd8e158f747ff58317ba1daeaeb91a709c186d66
stan-sisney <stan.sisney@gmail.com>
2024-04-18 13:29:58 -0500

    4-18

--- Async.py
+++ Async.py
@@ -4,32 +4,34 @@ from ollama import AsyncClient
 from pynput import keyboard
 from time import sleep
 
-global string 
+# declare my gloabl var
 string = ""
 
 #ollama.pull('mistral') #only needed if mistral is already dl
-async def chat():
-  global string
-  bigmsg = ""
+
+async def chat(): # async function
+  
+  global string  # ref the global variable 
+  string = ""  # pull it into the function
+  bigmsg = "" # string to be built up
   message = {'role': 'user', 'content': 'what is 3 +3?'}
+  
   async for part in await AsyncClient().chat(model='mistral', messages=[message], stream=True):
     
     if len(message) < 25:
         print('waiting')
         bigmsg = bigmsg + part['message']['content']
         if len(bigmsg) > 25:
-            #global string
-            #string = bigmsg
-            print(bigmsg)
-            bigmsg = ""
-            #string = ""
-            print('cleared')
-            #print(part['message']['content'], end='', flush=True)
-            #pass
-        #return string;        
+            string = bigmsg
+            print(string)
+            bigmsg = "" #empty big msg out                           
 asyncio.run(chat())
 
-def on_release(key):
+
+print("i am here too")
+
+
+"""async def on_release(key):
     if key == keyboard.Key.esc:
         return False
     
@@ -37,5 +39,8 @@ listener = keyboard.Listener(on_release=on_release)
 listener.start()
 
 while listener.is_alive():
-    print('key')
+    print("hit esc to stop")
+    print(string)
     sleep(5)
+
+asyncio.run(on_release)"""
