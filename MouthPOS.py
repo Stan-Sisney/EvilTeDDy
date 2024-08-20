03b0fc10e6350338b9cd660d43761f0831b02575
stan-sisney <stan.sisney@gmail.com>
2024-08-20 11:52:32 -0500

    8-20-24

--- MouthPOS.py
+++ MouthPOS.py
@@ -3,12 +3,21 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
+def n ():
+# lower jaw
+ kit.servo[8].angle = 0
+#upper Jaw
+ kit.servo[4].angle = 0
+
+n ()
+time.sleep(2)
+
 def cd ():
 # lower jaw
  kit.servo[8].angle = 90
 #upper Jaw
  kit.servo[4].angle = 180
 
-#cd ()
+cd ()
 
 #move serovs to x position
